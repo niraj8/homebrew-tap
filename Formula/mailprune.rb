ba1a611f3417e9d24ecce26becc91b1bf23865f2
Niraj Palecha <8666468+niraj8@users.noreply.github.com>
2026-08-09 12:24:10 +0530

    mailprune 0.2.1

--- Formula/mailprune.rb
+++ Formula/mailprune.rb
@@ -1,22 +1,22 @@
 class Mailprune < Formula
   desc "Email triage TUI - stack inbox by sender, bulk trash/archive/unsubscribe"
   homepage "https://github.com/niraj8/mailprune"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/niraj8/mailprune/releases/download/v0.2.0/mailprune-v0.2.0-aarch64-apple-darwin.tar.gz"
-      sha256 "ec0455eac57767645454cdde3cd96a41f9b2f6fe13652be445f39c43f955cc1b"
+      url "https://github.com/niraj8/mailprune/releases/download/v0.2.1/mailprune-v0.2.1-aarch64-apple-darwin.tar.gz"
+      sha256 "933c5d5af839cee3dbf504917dc0dd12e845d45276efba81523c83793ee35feb"
     else
-      url "https://github.com/niraj8/mailprune/releases/download/v0.2.0/mailprune-v0.2.0-x86_64-apple-darwin.tar.gz"
-      sha256 "a246dcc6fc2dd159597dd2609d450a2f81cd7539a1f033525da0d496c8ccf79a"
+      url "https://github.com/niraj8/mailprune/releases/download/v0.2.1/mailprune-v0.2.1-x86_64-apple-darwin.tar.gz"
+      sha256 "1f01fee97e795215fcfe3c20762c54cbe51d05758713e5118828a463b4178935"
     end
   end
 
   on_linux do
-    url "https://github.com/niraj8/mailprune/releases/download/v0.2.0/mailprune-v0.2.0-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "846c998b9996d9143b7050322698bb66bb3962822e14b22390456d24027fe863"
+    url "https://github.com/niraj8/mailprune/releases/download/v0.2.1/mailprune-v0.2.1-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "b93668de8a21b4bbb32bd5287746ba4c1acf27c50a3b97331de15b6a6c2e5641"
   end
 
   def install
